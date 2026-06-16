6bdf683561564a6c98f983429fae4b977ed32cc0
faujitec <faujitec@gmail.com>
2026-06-16 19:27:33 +0530

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,1 +1,22 @@
+import os
+import requests
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL = os.getenv("CHANNEL")
+
+
+def send_message(text):
+
+    url = (
+        f"https://api.telegram.org"
+        f"/bot{BOT_TOKEN}/sendMessage"
+    )
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHANNEL,
+            "text": text
+        },
+        timeout=30
+    )
