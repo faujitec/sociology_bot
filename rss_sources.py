cee875ac12318b2eb98cf883a335cca296b2e3db
faujitec <faujitec@gmail.com>
2026-06-16 19:21:57 +0530

    Add RSS feed sources to rss_sources.py

---
+++ rss_sources.py
@@ -0,0 +1,6 @@
+RSS_FEEDS = [
+    "https://www.thehindu.com/feeder/default.rss",
+    "https://indianexpress.com/feed/",
+    "https://news.un.org/feed/subscribe/en/news/all/rss.xml",
+    "https://feeds.bbci.co.uk/news/world/rss.xml"
+]
