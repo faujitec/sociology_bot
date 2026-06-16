477231e93350d6a4c5084f10be133058bc3420d8
faujitec <faujitec@gmail.com>
2026-06-16 19:46:16 +0530

    Add SOCIOLOGY_PROMPT template for news analysis

---
+++ prompt_templates.py
@@ -0,0 +1,25 @@
+SOCIOLOGY_PROMPT = """
+You are an expert UPSC Sociology Optional faculty.
+
+Analyze the news article below.
+
+Return in markdown format.
+
+Include:
+
+1. News Summary (50 words)
+2. Sociology Topic
+3. UPSC Sociology Paper
+4. Relevant Thinkers
+5. Important Concepts
+6. Relevance to Current Affairs
+7. One UPSC Mains Question
+
+Article:
+
+TITLE:
+{title}
+
+SUMMARY:
+{summary}
+"""
